5edd36d509788d2156fbae0cf65e2e63522ff853
Tobias Bitterli <tobias.bitterli@greenmatch.ch>
2018-12-03 23:33:59 +0100

    added struct for Attendees, added events, added withdraw function, added public view functions

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
 # smart-contracts-unibas
+
 Git Repo for smart contract lecture
 
 Voting on blockchain
 
-Functions:
-- Datum setzen (Nur durch Admin möglich)
-- Betrag setzen, der für den Event benötigt wird (nur admin)
-- Add Admin (Nur durch Owner möglich)
-- Member hinzufügen (alle Admin)
-- Für den Event anmelden (Für alle Member möglich) mit Geldüberweisung
-- Minimum Teilnehmer
-- Maximum Teilnehmer
-- Get Refund wenn zuwenige eingeschrieben
-- Get Refund wenn mehr Geld einbezahlt als benötigt
-
-Offene Fragen:
-- Was passiert wenn setEventDetails aufgerufen wird, nachdem bereits Geld einbezahlt wurde? --> abfangen
-- Withdraw for owner --> Automatische Auszahlung 
-- wenn Betrag nicht erreicht --> withdraw function für member, automatische auszahlung an alle
+Offene Punkte:
+- Remove attendees gelöscht --> unnötig
--- Voting.sol
+++ Voting.sol
@@ -1,23 +1,26 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.25;
 
 contract SmartVoting {
-    uint public totalAmountNeededForEvent = 0 ether;
+    uint public totalAmountNeededForEvent = 0;
     uint public minAttendees = 1;
     uint public maxAttendees = 1;
-    uint public numberOfAttendees = 0;
-    uint public pot = 0 ether;
-    uint public maxPaymentPerAttendee = 0 ether;
-    uint private repayment = 0;
-    bool public eventAlreadyExisting = false;
+    uint public maxPaymentPerAttendee = 0;
     uint64 public deadline;
+    uint private repayment = 0;
+    bool private eventAlreadyExisting = false;
     address owner = msg.sender;
-    address[] public attendees;
-    mapping (address => bool) public attendeesMapping;
+    address[] private attendeeAccounts;
+    mapping (address => Attendee) public attendees;
     mapping (address => bool) public admins;
     mapping (address => bool) public members;
-    mapping (address => uint) public accountBalance;
+    Attendee attendee;
 
+    struct Attendee {
+        uint balance;
+        bool hasSignedUp;
+    }
 
+    // Modifiers
     modifier onlyOwner{ 
         require(
             msg.sender == owner,
@@ -42,9 +45,22 @@ contract SmartVoting {
         _;
     }
 
+    // Events
+    event CreateEvent(
+        address indexed _from,
+        uint _amount,
+        uint _minAttendees,
+        uint _maxAttendees,
+        uint64 _deadline
+    );
+
+    event ConfirmEvent(
+        address indexed _from
+    );
+
     // Fallback function
-    function() public payable {
-        // Test
+    function() external payable {
+        
     }
 
     // Function to add a new member (only allowed by admins)
@@ -55,18 +71,6 @@ contract SmartVoting {
     // Function to remove a member (only allowed by admins)
     function removeMember(address _member) public onlyAdmins {
         members[_member] = false;
-
-        if(!admins[_member] && _member != owner) {
-            // fix this
-            _member.transfer(accountBalance[_member]);
-        }
-    }
-
-    function removeAttendee(address _attendee) public onlyOwner { 
-        // todo: fix this
-        _attendee.transfer(accountBalance[_attendee]);
-        // todo fix (Array)
-        attendees[_attendee] = false;
     }
 
     // Function to add a new admin (only allowed by owner)
@@ -77,66 +81,90 @@ contract SmartVoting {
     // Function to remove an admin (only allowed by owner)
     function removeAdmin(address _admin) public onlyOwner {
         admins[_admin] = false;
-
-        if(!members[_admin] && _admin != owner) {
-            _admin.transfer(accountBalance[_admin]);
-        }
     }
 
     // Function to set the amount that is required for the event
-    function createEvent(uint _amount, uint _minAttendees, uint _maxAttendees, uint _deadline) public onlyOwner {
+    function createEvent(uint _amount, uint _minAttendees, uint _maxAttendees, uint64 _deadline) public onlyOwner {
         if(!eventAlreadyExisting) {
-            numberOfAttendees = 0;
+            attendeeAccounts.length = 0;
             totalAmountNeededForEvent = _amount;
             minAttendees = _minAttendees;
             maxAttendees = _maxAttendees;
             deadline = _deadline;
             maxPaymentPerAttendee = totalAmountNeededForEvent/minAttendees;
             eventAlreadyExisting = true;
+
+            emit CreateEvent(msg.sender, _amount, _minAttendees, _maxAttendees, _deadline);
         }        
     }
 
-    function confirmEvent() public onlyOwner {
-        if(numberOfAttendees = maxAttendees || (block.number >= deadline && numberOfAttendees >= minAttendees)) {
-            repayment = (this.balance - totalAmountNeededForEvent) / numberOfAttendees;
+    function confirmEvent() public payable onlyOwner {
+        if(attendeeAccounts.length == maxAttendees || (block.number >= deadline && attendeeAccounts.length >= minAttendees)) {
+            uint256 costPerAttendee = totalAmountNeededForEvent / attendeeAccounts.length;
             msg.sender.transfer(totalAmountNeededForEvent);
             
-            // send difference back to all attendees
-            for(uint i = 0; i < attendees.length; i++) {
-                attendees[i].transfer(repayment);
+            // send difference back to all attendeeAccounts
+            for(uint i = 0; i < attendeeAccounts.length; i++) {
+                attendeeAccounts[i].transfer(attendees[attendeeAccounts[i]].balance - costPerAttendee);
+                attendees[attendeeAccounts[i]].balance = 0;
             }
-
             eventAlreadyExisting = false;
+            emit CreateEvent(msg.sender);
         }
     }
 
-    function cancelEvent() public onlyOwner {
-        repayment = this.balance / numberOfAttendees;
-        
-        // send funds back to all attendees
-        for(uint i = 0; i < attendees.length; i++) {
-            attendees[i].transfer(repayment);
+    function cancelEvent() public payable onlyOwner {
+        // send funds back to all attendeeAccounts
+        for(uint i = 0; i < attendeeAccounts.length; i++) {
+            attendeeAccounts[i].transfer(attendees[attendeeAccounts[i]].balance);
+            attendees[attendeeAccounts[i]].balance = 0;
         }
-
         eventAlreadyExisting = false;
     }
 
     // Function to sign up for the event
     function signUpForEvent() public payable onlyMembers {
-        if(msg.value >= maxPaymentPerAttendee && block.number < deadline && !attendees[msg.sender]) {
-            attendees[numberOfAttendees] = msg.sender;
-            attendeesMapping[msg.sender] = true;
-            numberOfAttendees += 1;
+        attendee = attendees[msg.sender];
+        if(msg.value >= maxPaymentPerAttendee && block.number < deadline && !attendee.hasSignedUp) {
+            attendee.hasSignedUp = true;
+            attendee.balance = maxPaymentPerAttendee;
+            attendeeAccounts.push(msg.sender);
+            msg.sender.transfer(msg.value - maxPaymentPerAttendee);
+        } else {
+            msg.sender.transfer(msg.value);
         }
     }
 
-    // Function to withdraw own amount
+    // Function to send funds back if minAttendees was not reached
     function withdraw() public onlyMembers {
-        if (block.number >= deadline && attendees[msg.sender] == true) {
-            uint _prePayment = totalAmountNeededForEvent/minAttendees;
-            uint _payback = _prePayment - totalAmountNeededForEvent/numberOfAttendees;
-            attendees[msg.sender] = false;
-            msg.sender.transfer(_payback);
+        if (block.number >= deadline && attendeeAccounts.length > minAttendees) {
+            // send funds back to all attendeeAccounts
+            for(uint i = 0; i < attendeeAccounts.length; i++) {
+                attendee = attendees[attendeeAccounts[i]];
+                attendeeAccounts[i].transfer(attendee.balance);
+                attendees[attendeeAccounts[i]].balance = 0;
+            }
+            eventAlreadyExisting = false;
         }
+    }
+
+    function countAttendees() public view returns (uint) {
+        return attendeeAccounts.length;
+    }
+
+    function isAdmin(address _address) public view returns (bool) {
+        return admins[_address];
+    }
+    
+    function isMember(address _address) public view returns (bool) {
+        return members[_address];
+    }
+
+    function isAttendee(address _address) public view returns (bool) {
+        return members[_address];
+    }
+
+    function showMyBalance() public view returns (uint256) {
+        return attendees[msg.sender].balance;
     }
 }
